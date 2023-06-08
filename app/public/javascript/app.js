e0306a2a4bafe4e246b83306ad4c0833be6eadea
grimario-andre <64281623+grimario-andre@users.noreply.github.com>
2023-06-08 14:24:38 -0300

    struct-tag-section-project-dinamic
    O portifolio ao longo tempo, serão adicionados projetos, na seção de projetos deve ser automatizado o processo de inseri-los. De alternativas de como fazer isso.
    
    Há várias alternativas para automatizar o processo de adicionar projetos à seção de projetos do seu portfólio. Aqui estão algumas opções:
    
    1 Utilizar uma plataforma de gerenciamento de conteúdo (CMS).
    
    2 Utilizar um gerador de sites estáticos.
    
    3 Utilizar uma linguagem de programação no lado do servidor.
    
    4 Utilizar uma API de terceiros.
    
    Sou iniciante, isto muito complexo. Preciso algo simples e funcional.
    É possível fazer isso clicando em um botao, abrir um modal com um formulario para inserir o conteudo da tag section?
    
    Uma solução mais simples e acessível para adicionar projetos ao seu portfólio, criar um formulário em um modal pode ser uma opção viável. Aqui está uma abordagem básica de como você pode implementar essa funcionalidade.
    
    Dessa forma, ao clicar no botão "Adicionar Projeto", o modal será aberto com um formulário para inserir os detalhes do projeto. Ao submeter o formulário, um novo elemento <section> será criado com as informações do projeto e adicionado à seção de projetos existente. O modal será fechado e os campos do formulário serão redefinidos para facilitar a inserção de projetos adicionais.

---
+++ app/public/javascript/app.js
@@ -0,0 +1,43 @@
+// Obtenha os elementos do DOM
+const addProjectButton = document.getElementById('addProjectButton');
+const projectModal = document.getElementById('projectModal');
+const closeButton = document.getElementsByClassName('close')[0];
+const projectForm = document.getElementById('projectForm');
+
+// Abrir o modal ao clicar no botão "Adicionar Projeto"
+addProjectButton.addEventListener('click', () => {
+  projectModal.style.display = 'block';
+});
+
+// Fechar o modal ao clicar no botão de fechar (X)
+closeButton.addEventListener('click', () => {
+  projectModal.style.display = 'none';
+});
+
+// Submeter o formulário ao adicionar um projeto
+projectForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  // Obter os valores dos campos do formulário
+  const projectName = document.getElementById('projectName').value;
+  const projectDescription = document.getElementById('projectDescription').value;
+  const projectLink = document.getElementById('projectLink').value;
+
+  // Criar um novo elemento section com os detalhes do projeto
+  const newProjectSection = document.createElement('section');
+  newProjectSection.innerHTML = `
+    <h2>${projectName}</h2>
+    <p>${projectDescription}</p>
+    <a href="${projectLink}">Ver projeto</a>
+  `;
+
+  // Adicionar o novo projeto à seção de projetos
+  const projectsSection = document.getElementById('projetos');
+  projectsSection.appendChild(newProjectSection);
+
+  // Limpar os campos do formulário
+  projectForm.reset();
+
+  // Fechar o modal
+  projectModal.style.display = 'none';
+});
